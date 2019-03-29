e660702f60d1e604b9fb8b0fee705587b805c38a
yoheikikuta <diracdiego@gmail.com>
2019-03-30 03:54:21 +0900

    Implement random number display
    When pushing the random button, we can see a random within
    0 to the count num.

--- app/src/main/java/my/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/my/example/myfirstapp/MainActivity.kt
@@ -3,9 +3,11 @@ package my.example.myfirstapp
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.annotation.IntegerRes
 import android.view.View
 import android.widget.TextView
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -38,6 +40,15 @@ class MainActivity : AppCompatActivity() {
     fun randomMe(view: View) {
         // Create an Intent to start the second activity.
         val randomIntent = Intent(this, SecondActivity::class.java)
+
+        // Get the current value of the text view.
+        val countString = textView.text.toString()
+
+        // Convert the count to an Int.
+        val count = Integer.parseInt(countString)
+
+        // Add the count to the extras for the Intent.
+        randomIntent.putExtra(SecondActivity.TOTAL_COUNT, count)
 
         // Start the new activity.
         startActivity(randomIntent)
